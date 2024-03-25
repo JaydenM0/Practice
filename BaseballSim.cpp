7dc4a3c6711d2081049e75ec7732bb4778b193af
JaydenM0 <131734946+JaydenM0@users.noreply.github.com>
2024-03-25 18:09:25 -0500

    Create BaseballSim.cpp

---
+++ BaseballSim.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include <string>
+#include <ctime>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+//make a baseball simulator
+
+/*
+create an array to replicate innings with 9 columns and 2 rows
+create 2 teams
+randomly generate a number for the top and the bottom innings
+the team with the larger score wins
+
+
+
+
+*/
+
+int innings[2][9] ={{0,0,0,0,0,0,0,0,0},
+                      {0,0,0,0,0,0,0,0,0}};
+
+void introduction(){
+    cout<<"Press Enter to Begin!"<<endl;
+    cin.ignore();
+
+    cout<<"-------------------------"<<endl;
+    cout<<"Welcome to Baseball Simulator!"<<endl;
+    cout<<"Home Team - Astros"<<endl;
+    cout<<"Away Team - Blue Jays"<<endl;
+
+};
+
+
+/*iterated through the array "innings" and disply the score at each element.*/
+
+void displayInnings(const int innings[2][9]) {
+    // Display the innings array
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 1; j < 10; ++j) {
+            cout << innings[i][j] << "|";
+        }
+        
+        cout << endl;
+        cout << "--------------------" << endl;
+    }
+    
+}
+
+
+/* calculate the final score of each team by iterating 
+    through the top of the innings then the bottom. 
+ */
+void finalScore(const int innings[2][9], int &team1Score, int &team2Score){
+    for(int i = 0; i < 2;++i){
+        for(int j = 0; j<10;++j){
+            if(i == 0){
+                team1Score += innings[i][j];
+            }
+            else{
+                team2Score += innings[i][j];
+            }
+        }
+    }
+};
+
+int main(){
+    introduction();
+    displayInnings(innings);
+    int inning = 1, turn = 1, runs, team1Score = 0, team2Score = 0;
+    //turn is used to determine if it is the top or bottom inning
+    
+    std::string team1 = "Astros";
+    std::string team2 = "Blue Jays";
+
+    srand(time(NULL)); //provide a random number when using "rand()"
+
+    while(inning != 10){
+        if(turn % 2 == 1){
+            cout<<"Top of the " << inning << " inning"<<endl;
+            runs = rand() % 10;
+            innings[0][inning] = runs;
+            displayInnings(innings);
+
+        }
+        else{
+            cout<<"Bottom of the "<< inning<<" inning"<<endl;
+            runs = rand() % 10;
+            innings[1][inning] = runs;
+            displayInnings(innings);      
+            inning++;
+        }
+    turn++;
+    }
+    finalScore(innings, team1Score, team2Score);
+    cout<<team1 <<": "<<team1Score<<"\n"<<team2<<": "<<team2Score<<endl;
+
+
+    if(team1Score > team2Score){
+        cout<<team1<<" wins!"<<endl;
+    }
+    else{
+        cout<<team2<<" wins!"<<endl;
+    }
+    cout<<team1Score<<" "<<team2Score<<endl;
+    return 0;
+};
